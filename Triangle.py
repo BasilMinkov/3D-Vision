4f866ea57ea4161d8c436de968da922c868765a5
Basil Minkov <proveyourselfmail@gmail.com>
2016-05-18 10:57:50 +0400

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -12,17 +12,17 @@ def plot_bars(n_groups, interps):
     plt.tight_layout()
     plt.show()
 
-def quart_f(zero, a, b, c, d):
+def quart_f(coef):
 
-    if zero != 0:
+    if coef[0] != 0:
 
-        a3 = float(a/zero)
-        a2 = float(b/zero)
-        a1 = float(c/zero)
-        a0 = float(d/zero)
+        a3 = float(coef[1]/coef[0])
+        a2 = float(coef[2]/coef[0])
+        a1 = float(coef[3]/coef[0])
+        a0 = float(coef[4]/coef[0])
 
         T1 = -a3/4
-        T2 = (a2 ** 2) - 3*a3*a1 + 12*a0
+        T2 = (a2**2) - 3*a3*a1 + 12*a0
         T3 = (2*(a2**3) - 9*a3*a2*a1 + 27*(a1**2) + 27*(a3**2)*a0 - 72*a2*a0)/2
         T4 = (-a3**3 + 4*a3*a2 - 8*a1)/32
         T5 = (3*(a3**2) - 8*a2)/48
@@ -32,8 +32,11 @@ def quart_f(zero, a, b, c, d):
         else:
             R1 = math.sqrt(T3**2 - T2**3)
 
-        R2 = (T3 + R1)**(1/3)
-        R3 = (1/12)*(T2/R2 + R2)
+        R2 = abs(T3 + R1)**(1.0/3) #Error
+        R3 = (1.0/12)*(T2/R2 + R2) #Error
+
+        if T3 + R1 < 0: #Error
+            R2 = -R2 #Error
 
         if T5 + R3 < 0:
             return []
@@ -41,26 +44,28 @@ def quart_f(zero, a, b, c, d):
             R4 = math.sqrt(T5 + R3)
 
         R5 = 2*T5 - R3
-        R6 = T4/R4
+
+        if abs(T4) < 1e-10 and abs(R4) < 1e-10:
+            R6 = 1
+            R4 = 0
+        else:
+            R6 = T4/R4
 
         answer = []
 
-        if R5 - R6 > -1:
-            r1 = T1 - R4 - math.sqrt(R5 - R6)
-            r2 = T1 - R4 + math.sqrt(R5 - R6)
-            answer.append(r1)
-            answer.append(r2)
+        if R5 - R6 >= 0: #-1: #Error
+            answer.append( T1 - R4 - math.sqrt(R5 - R6) )
+            answer.append( T1 - R4 + math.sqrt(R5 - R6) )
 
-        if R5 + R6 > -1:
-            r3 = T1 + R4 - math.sqrt(R5 + R6)
-            r4 = T1 + R4 + math.sqrt(R5 + R6)
-            answer.append(r3)
-            answer.append(r4)
+        if R5 + R6 >= 0: # -1: #Error
+            answer.append( T1 + R4 - math.sqrt(R5 + R6) )
+            answer.append( T1 + R4 + math.sqrt(R5 + R6) )
 
         return answer
 
-    if zero == 0:
-        return np.roots(a, b, c, d) # Here begins the cubic function.
+    if coef[0] == 0:
+        coef.remove(1)
+        return np.roots(coef) # Here begins the cubic function.
 
 def p_tetrahedron(angle_a, angle_b, angle_ab, angle_bc, angle_ca):
     # Valid tetrahedron
