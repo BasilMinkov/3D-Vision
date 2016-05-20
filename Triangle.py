5707e6d5027805110507f2c48ce4fce5ddf4c839
Basil Minkov <proveyourselfmail@gmail.com>
2016-05-20 20:55:15 +0300

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -2,88 +2,102 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+
 def plot_bars(n_groups, interps):
+    """ Returns a 15-interpretation bar plot for hist_list. Demands package matplotlib.pyplot as plt. """
+
     index = np.arange(n_groups)
     info = plt.bar(index, interps, width=1, alpha=0.6, color='r')
     plt.xlabel('Number Of Interpretations')
     plt.ylabel('Number Of Solutions')
     plt.title('Number Of Solutions For Each Number Of Interpretations')
-    plt.xticks(index+0.5, index)
+    plt.xticks(index + 0.5, index)
     plt.tight_layout()
     plt.show()
 
+
 def quart_f(coef):
+    """ Returns only real roots of the equation for maximum four coefficients.
+    Works much faster than numpy.roots() function as uses analytical method. """
 
     if coef[0] != 0:
 
-        a3 = float(coef[1]/coef[0])
-        a2 = float(coef[2]/coef[0])
-        a1 = float(coef[3]/coef[0])
-        a0 = float(coef[4]/coef[0])
+        a3 = float(coef[1] / coef[0])
+        a2 = float(coef[2] / coef[0])
+        a1 = float(coef[3] / coef[0])
+        a0 = float(coef[4] / coef[0])
 
-        T1 = -a3/4
-        T2 = (a2**2) - 3*a3*a1 + 12*a0
-        T3 = (2*(a2**3) - 9*a3*a2*a1 + 27*(a1**2) + 27*(a3**2)*a0 - 72*a2*a0)/2
-        T4 = (-a3**3 + 4*a3*a2 - 8*a1)/32
-        T5 = (3*(a3**2) - 8*a2)/48
+        T1 = -a3 / 4
+        T2 = (a2 ** 2) - 3 * a3 * a1 + 12 * a0
+        T3 = (2 * (a2 ** 3) - 9 * a3 * a2 * a1 + 27 * (a1 ** 2) + 27 * (a3 ** 2) * a0 - 72 * a2 * a0) / 2
+        T4 = (-a3 ** 3 + 4 * a3 * a2 - 8 * a1) / 32
+        T5 = (3 * (a3 ** 2) - 8 * a2) / 48
 
-        if T3**2 - T2**3 < 0:
+        if T3 ** 2 - T2 ** 3 < 0:
             return []
         else:
-            R1 = math.sqrt(T3**2 - T2**3)
+            R1 = math.sqrt(T3 ** 2 - T2 ** 3)
 
-        R2 = abs(T3 + R1)**(1.0/3) #Error
-        R3 = (1.0/12)*(T2/R2 + R2) #Error
+        R2 = abs(T3 + R1) ** (1.0 / 3)  # Error
 
-        if T3 + R1 < 0: #Error
-            R2 = -R2 #Error
+        if T3 + R1 < 0:  # Error
+            R2 = -R2  # Error
+
+        R3 = (1.0 / 12) * (T2 / R2 + R2)  # Error
 
         if T5 + R3 < 0:
             return []
         else:
             R4 = math.sqrt(T5 + R3)
 
-        R5 = 2*T5 - R3
+        R5 = 2 * T5 - R3
 
         if abs(T4) < 1e-10 and abs(R4) < 1e-10:
             R6 = 1
             R4 = 0
         else:
-            R6 = T4/R4
+            R6 = T4 / R4
 
         answer = []
 
-        if R5 - R6 >= 0: #-1: #Error
-            answer.append( T1 - R4 - math.sqrt(R5 - R6) )
-            answer.append( T1 - R4 + math.sqrt(R5 - R6) )
+        if R5 - R6 >= 0:  # -1: #Error
+            answer.append(T1 - R4 - math.sqrt(R5 - R6))
+            answer.append(T1 - R4 + math.sqrt(R5 - R6))
 
-        if R5 + R6 >= 0: # -1: #Error
-            answer.append( T1 + R4 - math.sqrt(R5 + R6) )
-            answer.append( T1 + R4 + math.sqrt(R5 + R6) )
+        if R5 + R6 >= 0:  # -1: #Error
+            answer.append(T1 + R4 - math.sqrt(R5 + R6))
+            answer.append(T1 + R4 + math.sqrt(R5 + R6))
 
         return answer
 
-    if coef[0] == 0:
-        coef.remove(1)
-        return np.roots(coef) # Here begins the cubic function.
+    if coef[0] == 0:  # Here begins the cubic function, that should be written after the conference.
+        roots = np.roots(coef)
+        list_x = []
+        for x in roots:
+            if x.imag == 0:
+                list_x.append(x.real)
+            return list_x
 
-def p_tetrahedron(angle_a, angle_b, angle_ab, angle_bc, angle_ca):
+
+def p_tet(angle_a, angle_b, angle_ab, angle_bc, angle_ca):
+    """ Returns number of solutions and type of solutions for the given tetrahedron angles."""
+
     # Valid tetrahedron
-    if angle_ab <= 0 or 180 <= angle_ab  or  angle_bc <= 0 or 180 <= angle_bc  or  angle_ca <= 0 or 180 <= angle_ca:
-        return [0,10]
+    if angle_ab <= 0 or 180 <= angle_ab or angle_bc <= 0 or 180 <= angle_bc or angle_ca <= 0 or 180 <= angle_ca:
+        return [0, 10]
 
-    if angle_ab+angle_bc < angle_ca or angle_bc+angle_ca < angle_ab or angle_ca+angle_ab < angle_bc:
-        return [0,10]
+    if angle_ab + angle_bc < angle_ca or angle_bc + angle_ca < angle_ab or angle_ca + angle_ab < angle_bc:
+        return [0, 10]
 
-    if angle_a <= 0 or 180 <= angle_a  or  angle_b <= 0 or 180 <= angle_b:
-        return [0,10]
+    if angle_a <= 0 or 180 <= angle_a or angle_b <= 0 or 180 <= angle_b:
+        return [0, 10]
 
     # Valid triangle
     if 180 <= angle_a + angle_b:
-        return [0,10]
+        return [0, 10]
 
     if 360 <= angle_ab + angle_bc + angle_ca:
-        return [0,10]
+        return [0, 10]
 
     angle_c = 180 - (angle_a + angle_b)
 
@@ -97,16 +111,16 @@ def p_tetrahedron(angle_a, angle_b, angle_ab, angle_bc, angle_ca):
     sinB = math.sin(math.radians(angle_b))
 
     # Valid triangle and valid image but invalid tetrahedron
-    if (180-angle_ab)+(180-angle_bc) < angle_b:
-        return [0,11]
-    if (180-angle_bc)+(180-angle_ca) < angle_c:
-        return [0,11]
-    if (180-angle_ca)+(180-angle_ab) < angle_a:
-        return [0,11]
+    if (180 - angle_ab) + (180 - angle_bc) < angle_b:
+        return [0, 11]
+    if (180 - angle_bc) + (180 - angle_ca) < angle_c:
+        return [0, 11]
+    if (180 - angle_ca) + (180 - angle_ab) < angle_a:
+        return [0, 11]
 
     Rab = 1
-    Rbc = Rab/(cosB+sinB*cosA/sinA)
-    Rca = Rab/(cosA+sinA*cosB/sinB)
+    Rbc = Rab / (cosB + sinB * cosA / sinA)
+    Rca = Rab / (cosA + sinA * cosB / sinB)
 
     K1 = (Rbc ** 2) / (Rca ** 2)
     K2 = (Rbc ** 2) / (Rab ** 2)
@@ -121,95 +135,88 @@ def p_tetrahedron(angle_a, angle_b, angle_ab, angle_bc, angle_ca):
     G0 = (K1 * K2 + K1 - K2) ** 2 - 4 * (K1 ** 2) * K2 * (cosCA ** 2)
 
     p = [G4, G3, G2, G1, G0]
-    g_roots = list(np.roots(p))
+    g_roots = quart_f(p)
 
     list_x = []
     for x in g_roots:
-        if x.imag == 0 and 0 < x.real:
+        if 0 < x.real:
             list_x.append(x.real)
 
-    if list_x == []:
+    if not list_x:  # if list_x is empty
         # Revise this "print" line.
         # print "Case A: (%d, %d, %d) and (%d, %d, %d)" % (angle_a, angle_b, angle_c, angle_ab, angle_bc, angle_ca)
-        return [0,20]
+        return [0, 20]
 
-    numSolutions = 0
-    typeSolutions = 0
+    num_sol = 0
+    type_sol = 0
     for x in list_x:
-
-        if x**2 - 2*x*cosAB + 1 <= 0:
+        if x ** 2 - 2 * x * cosAB + 1 <= 0:
             continue
 
-        a = Rab / math.sqrt(x**2 - 2*x*cosAB + 1)  # A24
-        #b = x * a
+        a = Rab / math.sqrt(x ** 2 - 2 * x * cosAB + 1)  # A24
+        # b = x * a
         # a > 0 and b > 0 because Rab > 0
 
-        m1 = 1-K1
-        p1 = 2*(K1*cosCA - x*cosBC)
-        q1 = (x**2 - K1)
+        m1 = 1 - K1
+        p1 = 2 * (K1 * cosCA - x * cosBC)
+        q1 = (x ** 2 - K1)
 
         m2 = 1
-        p2 = 2*(-x*cosBC)
-        q2 = x**2 * (1-K2) + 2*x*K2*cosAB - K2
+        p2 = 2 * (-x * cosBC)
+        q2 = x ** 2 * (1 - K2) + 2 * x * K2 * cosAB - K2
 
-        if m1*q2-m2*q1 != 0:
-            c = a * ((p2*q1 - p1*q2) / (m1*q2 - m2*q1))
+        if m1 * q2 - m2 * q1 != 0:
+            c = a * ((p2 * q1 - p1 * q2) / (m1 * q2 - m2 * q1))
             if c > 0:
-                numSolutions += 1
+                num_sol += 1
         else:
-            typeSolutions += 1
-            if (cosCA ** 2) + (((Rca ** 2) - (a ** 2)) / (a ** 2)) > 0: # it is, perhaps, always true (> 0)
+            type_sol += 1
+            if (cosCA ** 2) + (((Rca ** 2) - (a ** 2)) / (a ** 2)) > 0:  # it is, perhaps, always true (> 0)
                 y1 = cosCA + math.sqrt((cosCA ** 2) + (((Rca ** 2) - (a ** 2)) / (a ** 2)))  # A27
                 if y1 > 0:
-                    numSolutions += 1
-
+                    num_sol += 1
                 y2 = cosCA - math.sqrt((cosCA ** 2) + (((Rca ** 2) - (a ** 2)) / (a ** 2)))  # A27
                 if y2 > 0:
-                    numSolutions += 1
+                    num_sol += 1
             else:
                 continue
 
-    if numSolutions == 0:
+    if num_sol == 0:
         # Revise this "print" line.
         print("Case B: (%d, %d, %d) and (%d, %d, %d)") % (angle_a, angle_b, angle_c, angle_ab, angle_bc, angle_ca)
-        return [0,21]
-    return [numSolutions, typeSolutions]
+        return [0, 21]
+    return [num_sol, type_sol]
+
 
 def hist_base(tAngles, vAngles):
-    # hist_base = {a: 0 for a in range(0, 15)}
-    hist_base = [0 for a in range(0, 15)]
+    """ Returns numbers of each of 15 interpretations for the given intervals 
+    of tAngles (angles in the base of tetrahedron) and vAngles (visual angles at apex). """
+
+    hb = [0 for a in range(0, 15)]
     for angle_a in tAngles:
-        #print "angleA=%d" %angle_a
+        # print("angleA=%d") %angle_a
         for angle_b in tAngles:
-            #print "angleB=%d" %angle_b
+            # print("angleB=%d") %angle_b
+            if 180 <= angle_a + angle_b and angle_a < angle_b < (180 - angle_a - angle_b):
+                for angle_ab in vAngles:
+                    for angle_bc in vAngles:
+                        for angle_ca in vAngles:
+                            results = p_tet(angle_a, angle_b, angle_ab, angle_bc, angle_ca)
+                            hb[int(results[0])] += 1
+                            if results[1] == 1 or results[1] == 2:
+                                # Revise this "print" line.
+                                print("Case C: (%d, %d, %d) and (%d, %d, %d)") % (
+                                    angle_a, angle_b, (180 - angle_a - angle_b), angle_ab, angle_bc, angle_ca)
 
-            if 180 <= angle_a + angle_b:
-                continue
+    return hb
 
-            if angle_a < angle_b:
-                continue
 
-            if angle_b < (180 - angle_a - angle_b):
-                continue
-
-            for angle_ab in vAngles:
-                for angle_bc in vAngles:
-                    for angle_ca in vAngles:
-                        results = p_tetrahedron(angle_a, angle_b, angle_ab, angle_bc, angle_ca)
-                        hist_base[int(results[0])] += 1
-
-                        if results[1]==1 or results[1]==2:
-                            # Revise this "print" line.
-                            print("Case C: (%d, %d, %d) and (%d, %d, %d)") % (angle_a, angle_b, (180 - angle_a - angle_b), angle_ab, angle_bc, angle_ca)
-
-    return hist_base
-
-tAngles = range(1, 180, 1) # Triangle
+tAngles = range(1, 180, 1)  # Triangle
 vAngles = range(10, 20, 1)  # Visual angles at apex
 
-hb = hist_base(tAngles, vAngles)
-print(hb)
-plot_bars(len(hb), hb)
+hist_list = hist_base(tAngles, vAngles)
+print(hist_list)
+plot_bars(len(hist_list), hist_list)
 
 # An example from Fischler & Bolles (1981)
 # angle_abbcca = math.degrees(math.acos(5.0/8.0))
